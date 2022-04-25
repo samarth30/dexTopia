876cf9f530bff2bacfae396e5330ef4295208fce
arnav <arnavnegi2000@gmail.com>
2022-04-25 16:25:32 +0530

    ui changes

--- components/ssPools/poolsRow.js
+++ components/ssPools/poolsRow.js
@@ -390,6 +390,24 @@ export default function PoolsRow({
                     </Typography>
                    
                   </Grid>
+                  <Grid xs={12} lg={1.5} className={style.tableBox2}>
+                    <Typography variant="p" className={style.tableBox2text}>
+                      {poolStaked[index] &&
+                        formatCurrency(
+                          BigNumber(poolStaked[index][0]).div(10 ** 18)
+                        )}%
+                    </Typography>
+                   
+                  </Grid>
+                  <Grid xs={12} lg={1.5} className={style.tableBox2}>
+                    <Typography variant="p" className={style.tableBox2text}>
+                      ${poolStaked[index] &&
+                        formatCurrency(
+                          BigNumber(poolStaked[index][0]).div(10 ** 18)
+                        )}
+                    </Typography>
+                   
+                  </Grid>
                   
                   <Grid xs={12} lg={2} className={style.tableBox2}>
                     <Box style={{ marginLeft: "50px" }}>
